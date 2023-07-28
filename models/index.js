657b8172dad872a5eb121a782ada4c1d4723aac8
Chesney Julian <chesney.julian724@gmail.com>
2023-07-28 16:27:04 -0400

    add content to index.js in models to create connections between tables

--- models/index.js
+++ models/index.js
@@ -4,3 +4,30 @@
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
+
+// user has many posts
+User.hasMany(Post, {
+  foreignKey: 'creatorId',
+});
+// user has many comments
+User.hasMany(Comment, {
+  foreignKey: 'creatorId'
+});
+// post belongs to user
+Post.belongsTo(User, {
+  foreignKey: 'creatorId'
+});
+// comment belongs to user
+Comment.belongsTo(User, {
+  foreignKey: 'creatorId'
+});
+// post has many comments
+Post.hasMany(Comment, {
+  foreignKey: 'postId'
+});
+// comment belongs to post
+Comment.belongsTo(Post, {
+  foreignKey: 'postId'
+});
+
+module.exports = { User, Post, Comment };
