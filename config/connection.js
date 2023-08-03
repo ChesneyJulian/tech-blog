22e93d163535a8b2f79a26393a6a22290a4b0731
Chesney Julian <chesney.julian724@gmail.com>
2023-08-03 16:57:15 -0400

    add optional jawsdb env variables in connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,10 @@ const Sequelize = require('sequelize');
 // bring in dotenv and configure it to use for env variables
 require('dotenv').config();
 
-// set up new instance of sequelize passed initializers from dotenv
-const sequelize = new Sequelize(
+// set up new instance of sequelize to look for jawsdb and passed initializers from dotenv
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
